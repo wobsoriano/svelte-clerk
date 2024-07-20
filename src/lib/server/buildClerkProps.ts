3ee7b8bec5c4f54ff7c8ebb1537701a9c0c01021
wobsoriano <sorianorobertc@gmail.com>
2024-07-20 10:16:16 -0700

    chore: update buildClerkProps def

--- src/lib/server/buildClerkProps.ts
+++ src/lib/server/buildClerkProps.ts
@@ -7,7 +7,7 @@ import type { InitialState } from '@clerk/types';
 
 /**
  * To enable Clerk SSR support, include this object to the props
- * returned from `load`. This will automatically make the auth state available to
+ * returned from your layout's `load` function. This will make the auth state available to
  * the Clerk components and hooks during SSR, the hydration phase and CSR.
  * @example
  * import { buildClerkProps } from 'svelte-clerk/server';
