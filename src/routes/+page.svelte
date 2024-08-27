3907049bc31ea7230a24909232f6dbdcd6d75b26
wobsoriano <sorianorobertc@gmail.com>
2024-08-26 18:55:52 -0700

    Update playground

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1 class="text-2xl font-bold mb-5">Welcome</h1>
 <p class="mb-5">
-	This is the demo site for Traversy Media's Next.js & Clerk tutorial. Go ahead and sign up or sign
+	A demo playground for Svelte 5 + Clerk. Go ahead and sign up or sign
 	in!
 </p>
