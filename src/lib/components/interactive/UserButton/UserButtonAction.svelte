21a438ed87d89efd5d049cb2454f935030fce73b
wobsoriano <sorianorobertc@gmail.com>
2024-10-14 11:34:56 -0700

    chore: fix more types and add reordering

--- src/lib/components/interactive/UserButton/UserButtonAction.svelte
+++ src/lib/components/interactive/UserButton/UserButtonAction.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
-	import { getContext, onMount, mount, type Component, type Snippet, unmount } from 'svelte';
+	import { getContext, onMount, mount, type Component, unmount } from 'svelte';
 	import type { UserButtonContext } from './types';
 
 	const { addCustomMenuItem } = getContext<UserButtonContext>('$$_userButtonMenuItems');
 
-	const {
-		label,
-		onclick,
-		labelIcon
-	}: { children?: Snippet; label: string; onclick?: () => void; labelIcon?: Component } = $props();
+	const reorderItemsLabels = ['manageAccount', 'signOut'];
+	type ReorderItemsLabels = (typeof reorderItemsLabels)[number];
+	type BaseActionItem = { label: string; onclick: () => void; labelIcon: Component };
+
+	const props: { label: ReorderItemsLabels } | BaseActionItem = $props();
 
 	onMount(() => {
 		let app: Record<string, unknown>;
+		const isReorderItem = reorderItemsLabels.includes(props.label);
+
+		if (isReorderItem) {
+			addCustomMenuItem('action', {
+				label: props.label
+			});
+			return;
+		}
+
+		const { label, onclick, labelIcon } = props as BaseActionItem;
 		addCustomMenuItem('action', {
 			label,
 			mountIcon(el) {
-				app = mount(labelIcon as any, { target: el });
+				app = mount(labelIcon, { target: el });
 			},
 			unmountIcon() {
 				if (app) {
