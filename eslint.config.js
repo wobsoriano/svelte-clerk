64395aadfb270001512f619eca4f17ac96700420
wobsoriano <sorianorobertc@gmail.com>
2024-10-24 15:57:57 -0400

    chore: update ignored folders

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,6 @@ export default ts.config(
 		}
 	},
 	{
-		ignores: ['**/build/', '**/.svelte-kit/', '**/dist/']
+		ignores: ['**/build/', '**/.svelte-kit/', '**/dist/', 'apps/docs/.vitepress/cache/']
 	}
 );
