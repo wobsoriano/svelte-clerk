f13834f3d1c78b7c25c88f629212da481757e176
wobsoriano <sorianorobertc@gmail.com>
2024-10-28 08:37:34 -0700

    fix: make sure env vars exist before accessing it

--- packages/svelte-clerk/svelte.config.js
+++ packages/svelte-clerk/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
