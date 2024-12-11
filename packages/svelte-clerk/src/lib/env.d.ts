d730beb05b57d7e1aa4c2b1411fc1ba97859c07a
wobsoriano <sorianorobertc@gmail.com>
2024-12-10 20:40:58 -0800

    feat: add current user helper

--- packages/svelte-clerk/src/lib/env.d.ts
+++ packages/svelte-clerk/src/lib/env.d.ts
@@ -1,9 +1,10 @@
-import type { AuthObject } from '@clerk/backend';
+import type { AuthObject, User } from '@clerk/backend';
 
 declare global {
 	namespace App {
 		interface Locals {
 			auth: AuthObject;
+			currentUser: () => Promise<User | null>;
 		}
 	}
 }
