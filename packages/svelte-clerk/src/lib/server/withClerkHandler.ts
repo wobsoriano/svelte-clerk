c9ec3b751d17d4d3b0b19d669dc14e8453728592
wobsoriano <sorianorobertc@gmail.com>
2024-12-14 09:59:33 -0800

    chore: clean up server handler

--- packages/svelte-clerk/src/lib/server/withClerkHandler.ts
+++ packages/svelte-clerk/src/lib/server/withClerkHandler.ts
@@ -1,4 +1,4 @@
-import { type Handle } from '@sveltejs/kit';
+import { type Handle, type RequestEvent } from '@sveltejs/kit';
 import { clerkClient } from './clerkClient.js';
 import * as constants from './constants.js';
 import {
@@ -8,6 +8,7 @@ import {
 } from '@clerk/backend/internal';
 import { parse } from 'set-cookie-parser';
 import { createCurrentUser } from './currentUser.js';
+import type { AuthObject } from '@clerk/backend';
 
 export type ClerkSvelteKitMiddlewareOptions = AuthenticateRequestOptions & { debug?: boolean };
 
@@ -39,29 +40,36 @@ export function withClerkHandler(middlewareOptions?: ClerkSvelteKitMiddlewareOpt
 		}
 
 		const authObject = requestState.toAuth();
-		event.locals.auth = authObject;
-		event.locals.currentUser = createCurrentUser(authObject);
+		decorateLocals(event, authObject)
+
 		if (debug) {
 			console.log('[svelte-clerk] ' + JSON.stringify(authObject));
 		}
 
-		type CookieSerializerOptions = Parameters<typeof event.cookies.set>[2];
-
-		if (requestState.headers) {
-			const setCookie = requestState.headers.get('set-cookie');
-			// We separate cookie setting logic because SvelteKit
-			// does not allow setting cookies with setHeaders.
-			if (setCookie) {
-				const parsedCookies = parse(setCookie);
-				parsedCookies.forEach((parsedCookie) => {
-					const { name, value, ...options } = parsedCookie;
-					event.cookies.set(name, value, options as CookieSerializerOptions & { path: string });
-				});
-				requestState.headers.delete('set-cookie');
-			}
-			event.setHeaders(Object.fromEntries(requestState.headers));
-		}
+    decorateHeaders(event, requestState.headers);
 
 		return resolve(event);
 	};
+}
+
+function decorateHeaders(event: RequestEvent, headers: Headers) {
+	type CookieSerializerOptions = Parameters<typeof event.cookies.set>[2];
+
+  const setCookie = headers.get('set-cookie');
+	// We separate cookie setting logic because SvelteKit
+	// does not allow setting cookies with setHeaders.
+	if (setCookie) {
+		const parsedCookies = parse(setCookie);
+		parsedCookies.forEach((parsedCookie) => {
+			const { name, value, ...options } = parsedCookie;
+			event.cookies.set(name, value, options as CookieSerializerOptions & { path: string });
+		});
+		headers.delete('set-cookie');
+	}
+	event.setHeaders(Object.fromEntries(headers));
+}
+
+function decorateLocals(event: RequestEvent, authObject: AuthObject) {
+  event.locals.auth = authObject;
+  event.locals.currentUser = createCurrentUser(authObject);
 }
