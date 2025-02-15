a7b2ba8486d50a412da1af59ac169bdd2ad638db
wobsoriano <sorianorobertc@gmail.com>
2025-02-14 20:43:32 -0800

    chore: add mergeWithPublicEnvVariables JSdoc

--- src/lib/utils/mergeWithPublicEnvVariables.ts
+++ src/lib/utils/mergeWithPublicEnvVariables.ts
@@ -1,6 +1,13 @@
 import type { LoadClerkJsScriptOptions } from '@clerk/shared';
 import { getStaticPublicEnvVariables } from './getStaticPublicEnvVariables.js';
 
+/**
+ * Merges the public environment variables with the Clerk initialization options.
+ *
+ * This function takes a Clerk initialization options object and returns a new object
+ * with the public environment variables merged into it. If a property is already
+ * defined in the Clerk initialization options, it will not be overridden.
+ */
 export function mergeWithPublicEnvVariables(
 	clerkInitOptions: Omit<LoadClerkJsScriptOptions, 'publishableKey'> & {
 		publishableKey?: string;
