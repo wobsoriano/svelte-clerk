cce77665e012c418e0e375d473c6782b52677784
wobsoriano <sorianorobertc@gmail.com>
2025-03-14 12:48:21 -0700

    add vanilla custom page in playground

---
+++ src/components/CustomPage.svelte
@@ -0,0 +1,4 @@
+<div>
+	<h1><b>Custom Page</b></h1>
+	<p>This is the content of the custom page.</p>
+</div>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,5 +1,39 @@
 <script lang="ts">
 	import { UserProfile } from '$lib/client';
+	import { mount, onMount, unmount, type Snippet } from 'svelte';
+	import type { CustomPage } from '@clerk/types';
+	import Portal from '$lib/client/Portal.svelte';
+	import CustomPageComponent from '../../components/CustomPage.svelte';
+
+	let customPages = $state<CustomPage[]>([]);
+
+	onMount(() => {
+		let page: Record<string, unknown>;
+
+		customPages.push({
+			url: 'custom-page',
+			label: 'Custom Page',
+			mountIcon: (el) => {
+				el.innerHTML = '👋';
+			},
+			unmountIcon: (el) => {
+				if (el) {
+					el.innerHTML = '';
+				}
+			},
+			mount: (el) => {
+				page = mount(Portal, {
+					target: el,
+					props: { children: CustomPageComponent as unknown as Snippet }
+				});
+			},
+			unmount: () => {
+				if (page) {
+					unmount(page);
+				}
+			}
+		});
+	});
 </script>
 
-<UserProfile />
+<UserProfile {customPages} />
