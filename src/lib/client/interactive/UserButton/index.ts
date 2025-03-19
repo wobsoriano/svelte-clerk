10cf836edcef037e765279420395bfa89764d650
wobsoriano <sorianorobertc@gmail.com>
2025-03-18 18:55:23 -0700

    chore: Remove manual typing of UserButton component

--- src/lib/client/interactive/UserButton/index.ts
+++ src/lib/client/interactive/UserButton/index.ts
@@ -4,16 +4,7 @@ import UserButtonLink from './UserButtonLink.svelte';
 import UserButtonMenuItems from './UserButtonMenuItems.svelte';
 import UserButtonUserProfilePage from './UserButtonUserProfilePage.svelte';
 
-// Temporary fix to "cannot be named" issue when building d.ts.
-// See https://github.com/sveltejs/svelte/discussions/12481
-type UserButtonType = typeof _UserButton & {
-	MenuItems: typeof UserButtonMenuItems;
-	Action: typeof UserButtonAction;
-	Link: typeof UserButtonLink;
-	UserProfilePage: typeof UserButtonUserProfilePage;
-};
-
-const UserButton: UserButtonType = Object.assign(_UserButton, {
+const UserButton = Object.assign(_UserButton, {
 	MenuItems: UserButtonMenuItems,
 	Action: UserButtonAction,
 	Link: UserButtonLink,
