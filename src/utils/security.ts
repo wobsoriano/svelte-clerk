de11b8cd335c1df5300557351f6152ebfcb0aaa2
wobsoriano <sorianorobertc@gmail.com>
2025-06-06 11:32:54 -0700

    fix type error

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,31 +1,41 @@
 import { error, redirect, type RequestEvent } from '@sveltejs/kit';
-import type { AuthObject } from '@clerk/backend';
+import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/internal';
+
+type SessionAuthObject = SignedInAuthObject | SignedOutAuthObject;
 
 export class Security {
-	private readonly auth?: AuthObject;
+	private readonly auth: SessionAuthObject;
 
 	constructor(private readonly event: RequestEvent) {
 		this.auth = event.locals.auth();
 	}
 
 	isPublic() {
-		if (this.auth?.userId) {
+		if (this.auth.userId) {
 			redirect(307, '/profile');
 		}
 		return this;
 	}
 
 	isAuthenticated() {
-		if (!this.auth?.userId) {
+		if (!this.auth.userId) {
 			redirect(307, '/sign-in');
 		}
 		return this;
 	}
 
 	hasPermission(permission: string) {
-		const permitted = this.auth?.has({ permission });
+		const permitted = this.auth.has({ permission });
 		if (!permitted) {
 			error(403, 'missing permission: ' + permission);
+		}
+		return this;
+	}
+
+	hasRole(role: string) {
+		const permitted = this.auth.has({ role });
+		if (!permitted) {
+			error(403, 'missing role: ' + role);
 		}
 		return this;
 	}
