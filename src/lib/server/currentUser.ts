50935dab1a30660c4e532286b19f2b9f091fd0a7
wobsoriano <sorianorobertc@gmail.com>
2025-06-22 11:06:37 -0700

    chore: add currentUser deprecation

--- src/lib/server/currentUser.ts
+++ src/lib/server/currentUser.ts
@@ -1,9 +1,11 @@
 import type { User } from '@clerk/backend';
 import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/internal';
 import { clerkClient } from './clerkClient.js';
+import { deprecated } from '@clerk/shared/deprecated';
 
 export function createCurrentUser(auth: SignedInAuthObject | SignedOutAuthObject) {
 	return async (): Promise<User | null> => {
+	  deprecated('currentUser()', 'Use `clerkClient.users.getUser(id)` instead.')
 		if (!auth.userId) {
 			return null;
 		}
