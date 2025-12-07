b685f9e86d3374e4dd79e32ce39dcc528f430815
wobsoriano <sorianorobertc@gmail.com>
2025-12-06 22:31:38 -0800

    chore: init from actions

--- src/lib/client/interactive/SignIn.svelte
+++ src/lib/client/interactive/SignIn.svelte
@@ -2,6 +2,7 @@
 	import { clerkHostRenderer } from '$lib/action.js';
 	import type { SignInProps } from '@clerk/shared/types';
 	import ClerkLoaded from '$lib/client/control/ClerkLoaded.svelte';
+	import { fromAction } from 'svelte/attachments';
 
 	const props: SignInProps = $props();
 </script>
@@ -9,12 +10,12 @@
 <ClerkLoaded>
 	{#snippet children(clerk)}
 		<div
-			use:clerkHostRenderer={{
-				mount: clerk.mountSignIn,
-				unmount: clerk.unmountSignIn,
-				updateProps: (clerk as any).__unstable__updateProps,
-				props: $state.snapshot(props)
-			}}
+			{@attach fromAction(clerkHostRenderer, () => ({
+               	mount: clerk.mountSignIn,
+               	unmount: clerk.unmountSignIn,
+               	updateProps: (clerk as any).__unstable__updateProps,
+               	props: $state.snapshot(props)
+            }))}
 		></div>
 	{/snippet}
 </ClerkLoaded>
